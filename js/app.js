c584288666abcee42c38a6bbc92fd0c283c415d4
Owen Flannigan <owenflannigan@Students-MacBook-Pro.local>
2016-11-03 13:53:46 -0700

    Fix play/pause button icon behavior

--- js/app.js
+++ js/app.js
@@ -2,16 +2,18 @@
 
 var searchForm = document.querySelector('#search form');
 
-searchForm.addEventListener('submit', function(event) {
+searchForm.addEventListener('submit', function (event) {
 	event.preventDefault();
+	adjustPlaylistHeight();
+	$(window).resize(adjustPlaylistHeight);
 
 	var input = $(searchForm).children('input').val();
 
 	var url = "https://api.spotify.com/v1/search?type=album,track,artist&q=" + input;
 
-	fetch(url).then(function(response) {
+	fetch(url).then(function (response) {
 		return response.json();
-	}).then(function(data) {
+	}).then(function (data) {
 		// data.tracks.items.forEach(function(item) {
 		// 	console.log(item.name + " by " + item.artists[0].name);
 		// });
@@ -19,6 +21,7 @@ searchForm.addEventListener('submit', function(event) {
 
 		// base curation off of song or album)
 		showCuratedSearch(data.tracks.items[0]);
+
 	});
 
 	return false; // for IE
@@ -35,7 +38,7 @@ function showPlaylist(songs) {
 	var body = $('#playlist-table tbody');
 	body.empty();
 
-	songs.forEach(function(song) {
+	songs.forEach(function (song) {
 		var row = $('<tr>');
 		console.log(song);
 
@@ -50,20 +53,13 @@ function showPlaylist(songs) {
 		// Sample song on click
 
 		// NEED TO FIX PAUSE PLAY TOGGLE ICONS
-		$(playButton).click(function() {
-			$(this).children('i').toggleClass('fa-play');
-			$(this).children('i').toggleClass('fa-pause');
-			$('tbody').children('td').each(function(td) {
-				if(td.children('i').hasClass('fa-pause')) {
-					td.children('i').toggleClass('fa-pause');
-					td.children('i').toggleClass('fa-play');
-				}
-			});
+		$(playButton).click(function () {
 			sampleSong(song);
+			toggleControlIcon(playButton);
 		});
 
 		var artistElem = $('<td headers="artists">');
-		artistElem.text(song.artists.map(function(artist) {
+		artistElem.text(song.artists.map(function (artist) {
 			return artist.name;
 		}).join(', '));
 
@@ -80,21 +76,45 @@ function showPlaylist(songs) {
 
 		body.append($(row));
 	});
-	
+
+}
+
+function adjustPlaylistHeight() {
+	var height = $(window).height() - $('#content').offset().top;
+	$('#playlist').height(height);
 }
 
 var audio = new Audio();
 
 function sampleSong(song) {
-	if(audio.src === song.preview_url) {
-		if(audio.paused)
+	if (audio.src === song.preview_url) { // same song
+		if (audio.paused) { // if paused, play
 			audio.play();
-		else
+			audio.volume = 0;
+			$(audio).animate({ volume: 1 }, 500); // fade in
+		} else {
 			audio.pause();
-	} else {
+		}
+	} else { // different song, or new song
 		audio.pause();
 		audio = new Audio(song.preview_url);
 		audio.play();
+		audio.volume = 0;
+		$(audio).animate({ volume: 1 }, 500); // fade in
 	}
-	
+}
+
+function toggleControlIcon(playButton) {
+	audio.onplay = function () {
+		$(playButton).children('i').toggleClass('fa-pause');
+		$(playButton).children('i').toggleClass('fa-play');
+	}
+	audio.onpause = function () {
+		$(playButton).children('i').toggleClass('fa-pause');
+		$(playButton).children('i').toggleClass('fa-play');
+	}
+	audio.onended = function () {
+		$(playButton).children('i').toggleClass('fa-pause');
+		$(playButton).children('i').toggleClass('fa-play');
+	}
 }
