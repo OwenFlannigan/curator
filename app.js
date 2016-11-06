1a379876b71c7c273819a634fce9d263d7113859
Owen Flannigan <owenflannigan@Students-MacBook-Pro.local>
2016-11-05 20:33:07 -0700

    Add initial curate request response

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ var curate = require('./routes/curate');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
--- routes/curate.js
+++ routes/curate.js
@@ -1,10 +1,52 @@
 var express = require('express');
+var request = require('request');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
-    var authentication = window.btoa();
+var clientID = '09031e5e116b41058b8c32ce8d4c882c';
+var secret = 'c13c3a5aee31458e841316192545e5f8';
 
-    res.send('success!');
+router.get('/', function(req, response, next) {
+    var authentication = new Buffer(clientID + ':' + secret).toString('base64');
+    var seeds = req.param('seed_id');
+
+    if(!(seeds)) {
+        response.send('Request is missing seed ID parameter');
+        return;
+    }
+
+    var authOptions = {
+        url: 'https://accounts.spotify.com/api/token',
+        form: {
+            grant_type: 'client_credentials'
+        },
+        headers: {
+            'Authorization': 'Basic ' + (authentication)
+        },
+        json: true
+    };
+
+    request.post(authOptions, function(err, res, body) {
+        if(!err && res.statusCode === 200) {
+            var token = body.access_token;
+
+            var reqOptions = {
+                url: 'https://api.spotify.com/v1/recommendations?seed_tracks=' + seeds,
+                headers: {
+                    'Authorization': 'Bearer ' + token
+                },
+                json: true
+            };
+
+            request.get(reqOptions, function(err, res, body) {
+                response.send(body);
+            });
+
+        } else {
+            console.log('error requesting auth', err);
+            response.send('Authorization request failed. Please contact the site administration to resolve this issue.');
+        }
+    });
+
 });
 
 module.exports = router;
