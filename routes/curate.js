72bc518cdeb1aaff23bc6895bda3de95e9e37d0c
Owen Flannigan <owenflannigan@Students-MacBook-Pro.local>
2016-11-06 18:59:12 -0800

    Hide client ID and secret

--- routes/curate.js
+++ routes/curate.js
@@ -1,17 +1,21 @@
 var express = require('express');
 var request = require('request');
 var router = express.Router();
+var fs = require('fs');
 
-var clientID = '09031e5e116b41058b8c32ce8d4c882c';
-var secret = 'c13c3a5aee31458e841316192545e5f8';
+var authentication = fs.readFileSync('client_info.txt', 'ascii', (err, data) => {
+    if(err) { throw err; }
+    return data;
+});
 
 router.get('/', function(req, response, next) {
-    var authentication = new Buffer(clientID + ':' + secret).toString('base64');
+    authentication = new Buffer(authentication).toString('base64');
     var seeds = req.query.seed_id;
-    console.log(seeds);
 
     if(!(seeds)) {
-        response.send('Request is missing seed ID parameter');
+        response.send({
+            'error': 'Request is missing seed ID parameter'
+        });
         return;
     }
 
@@ -44,7 +48,9 @@ router.get('/', function(req, response, next) {
 
         } else {
             console.log('error requesting auth', err);
-            response.send('Authorization request failed. Please contact the site administration to resolve this issue.');
+            response.send({
+                'error': 'Authorization request failed. Please contact the site administration to resolve this issue.'
+            });
         }
     });
 
